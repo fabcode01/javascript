77fe5c80fb76188f92c10dc4f78b5f447f340f9a
Fabrício Silva <114118215+fabcode01@users.noreply.github.com>
2023-01-03 23:20:10 -0300

    ex015

--- exercicios/ex014.js
+++ exercicios/ex014.js
@@ -8,5 +8,5 @@ while (c <= 6){
 
 do{
     console.log(`Passo ${c}`)
-    c++
-} while(c <= 6)
+    c += 1
+} while(c <= 10)
---
+++ exercicios/ex015.js
@@ -0,0 +1,8 @@
+console.log(`Vai começar!`)
+for (var c = 10; c >= 1; c--){
+    console.log(c)
+    
+
+}
+
+console.log('Começou!!!')
