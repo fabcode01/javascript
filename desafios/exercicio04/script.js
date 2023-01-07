64c9dc99a36d209c408d691ccc4cb8a067dc91b1
Fabrício Silva <fabricio85silva@hotmail.com>
2023-01-07 18:51:20 -0300

    exercicio04

---
+++ desafios/exercicio04/script.js
@@ -0,0 +1,29 @@
+function tabuada(){
+    let num = document.querySelector('input#txtnum')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {      
+        tab.innerHTML = ''
+                
+       let n = num.value
+       
+
+       for(let c = 1;c <= 10;c++){
+        let item = document.createElement('option')
+        
+        
+        item.text = `${n} x ${c} = ${n * c}`
+        tab.appendChild(item)
+       }
+        
+
+
+        
+        
+    }
+    }
+
+    
+
