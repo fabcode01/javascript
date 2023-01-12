a21216bd20329bd0cc8789c4d6f34815694c3d9f
Fabrício Silva <fabricio85silva@hotmail.com>
2023-01-11 21:50:02 -0300

    exercicio05

--- desafios/exercicio05/script.js
+++ desafios/exercicio05/script.js
@@ -1,9 +1,7 @@
 
 let num = document.getElementById('txtnum')
-
-let sel = document.getElementById('select#sel')
-
 let res = document.querySelector('div#res')
+let lista = document.querySelector('select#flista')
 
 let valores = []
 
@@ -26,13 +24,59 @@ function inList(n, l){
 
 function adicionar(){
     if (addNum(num.value) && !inList(num.value, valores)){
-      alert('tudo ok')
+    
+      valores.push(Number(num.value))
+      
+      let item = document.createElement('option')
+      
+      item.text = `Valor ${num.value}`
+
+      lista.appendChild(item)
+      res.innerHTML = ''
         
 
     }else {
         alert('Valor em branco ou já adicionado!')
     }
+
+    num.value = ''
+    num.focus()
  
     } 
+
+    function finalizar() {
+        if (valores.length == 0){
+            alert('A lista está vazia')
+        } else {
+            let total = valores.length
+            let maior = valores[0]
+            let menor = valores[0]
+            let soma = 0
+            let media = 0
+            
+
+            for(let pos in valores){
+                soma += valores[pos]
+              
+            }
+
+            media = soma / total
+            
+
+
+            res.innerHTML = ''
+            res.innerHTML += `<p> Ao todo temos <strong>${total}</strong> números cadastrados </p>`
+            
+            res.innerHTML += `<p>A soma dos elementos é: <strong>${soma}</strong></p>`
+            
+            res.innerHTML += `A média dos valores é: <strong>${media}</strong>`
+        }
+    }
       
 
+    function limpar(){
+        valores.length = 0
+        lista.length = 0
+        res.innerHTML = ''
+    }
+    
